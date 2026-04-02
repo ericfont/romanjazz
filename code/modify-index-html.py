b055745660cbe4bb6784d4788a84b7b537714d52
Eric Fontaine <ericfontainejazz@gmail.com>
2026-04-02 01:14:58 -0400

    try to copy  tabs to html

--- code/modify-index-html.py
+++ code/modify-index-html.py
@@ -8,10 +8,16 @@ shell=True, capture_output=True, text=True)
 
 revision_string = revision_command_result.stdout
 
-with open('code/scroll-list-display.html', 'r') as input_index_html, open('site/index.html', 'w') as output_index_html:
+with open('code/scroll-list-display.html', 'r') as input_index_html, open('all-charts-tabs.txt', 'r') as input_all_charts_tabs, open('site/index.html', 'w') as output_index_html:
     for line in input_index_html:
 
         if "<script>" in line:
-            output_index_html.write(f"<p>{revision_string}</p>\n")
+            output_index_html.write(f"<p>{revision_string}</p>\n<script>\nconst charts = [\n")
 
-        output_index_html.write(line)
+            for line2 in input_all_charts_tabs:
+                output_index_html.write(line2)
+            
+            output_index_html.write("[`END`,``]];\n")
+
+        else:
+            output_index_html.write(line)
