4ea9fcccb957bfbe3430c7716fce095c504536be
Eric Fontaine <ericfontainejazz@gmail.com>
2026-04-02 01:37:45 -0400

    Update modify-index-html.py

--- code/modify-index-html.py
+++ code/modify-index-html.py
@@ -11,8 +11,11 @@ revision_string = revision_command_result.stdout
 with open('code/scroll-list-display.html', 'r') as input_index_html, open('all-charts-tabs.txt', 'r') as input_all_charts_tabs, open('site/index.html', 'w') as output_index_html:
     for line in input_index_html:
 
+        if "<head>" in line:
+            output_index_html.write(f"<head><title>RomanJazz.com chord charts {revision_string}</title>")
+
         if "<script>" in line:
-            output_index_html.write(f"<p>{revision_string}</p>\n<script>\nconst charts = [\n")
+            output_index_html.write(f"<script>\nconst charts = [\n")
 
             for line2 in input_all_charts_tabs:
                 output_index_html.write(line2)
